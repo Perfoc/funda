5f0a0b8a0fb29848adf6e1968de0a175587786b8
Perfoc <49251191+Perfoc@users.noreply.github.com>
2019-04-08 18:57:10 +0300

    Create zarat.js

---
+++ komutlar/zarat.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+const fs = require("fs");
+exports.run = (client, message, params) => {
+var Random = [
+':one:',
+':two:',
+':three:',
+':four',
+':five',
+':six:',
+];
+var zaratt = Math.floor(Math.random()*Random.length);
+const zarat= new Discord.RichEmbed()
+.setDescription(`${Random[zaratt]}`)
+.setColor(0xe2ff00)
+.setTimestamp()
+message.channel.send(zarat)
+};
+exports.conf = {
+enabled: true,
+guildOnly: false,
+aliases: [],
+permLevel: 0
+};
+
+exports.help = {
+name: 'zarat',
+description: 'Zar atmaya yarar. (1 ve 6 arası)',
+usage: 'zatat'
+};
