4a00e81a912a0b64c81923b3607a06548bbcb1d1
Perfoc <49251191+Perfoc@users.noreply.github.com>
2019-04-09 00:33:13 +0300

    Add files via upload

--- komutlar/yazıtura.js
+++ komutlar/yazıtura.js
@@ -3,28 +3,28 @@ const chancejs = require('chance');
 const chance = new chancejs();
 
 const cevaplar = [
-	"YAZI-TURA:__TURA__**",
-	"YAZI-TURA:__YAZI__**"
+	"**__TURA__**",
+	"**__YAZI__**"
 ];
 
 exports.run = function(client, message) {
 	
 	var cevap = cevaplar[Math.floor(Math.random() * cevaplar.length)];
 	
-	if (cevap === "YAZI-TURA:__YAZI__**") {
+	if (cevap === "**__YAZI__**") {
 		
 		 const embedyazı = new Discord.RichEmbed()
 		.setColor(0xf4b942)
-		.setDescription(cevap)
-		.setThumbnail("http://www.freakonomics.com/wp-content/uploads/2013/01/coin-300x218.jpg")
+		//.setDescription()
+		.setThumbnail("https://image.prntscr.com/image/nSAgGoOmTii10YWD34WEZg.png")
 		message.channel.send(embedyazı);
 		
-	} else if (cevap === "YAZI-TURA:__TURA__**") {
+	} else if (cevap === "**__TURA__**") {
 		
 		const embedtura = new Discord.RichEmbed()
 		.setColor(0xf4b942)
-		.setDescription(cevap)
-		.setThumbnail("https://www.wallets-online.com/webadmin/items/ogon/ogon_cd_red_1.jpg")
+		//.setDescription()
+		.setThumbnail("https://image.prntscr.com/image/EHWJ3FtPSHus-h-ABhYy8g.png")
 		message.channel.send(embedtura);
 		
 	}
