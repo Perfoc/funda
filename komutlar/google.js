177d850ee5acd1d786b3c17ee3e397b56b2a7440
Perfoc <49251191+Perfoc@users.noreply.github.com>
2019-04-09 21:27:08 +0300

    Add files via upload

--- komutlar/google.js
+++ komutlar/google.js
@@ -7,10 +7,10 @@ exports.run = (client, message) => {
         message.channel.send({
             embed: {
                 color: 0xff2727,
-                description: `:warning: **${message.author.username}**, You didn't give me anything to search. {m!google \`input\`}`,
-                footer: {
+                description: `:warning: **${message.author.username}**, Komutun doğru kullanımı: {?google \`aranacak kelime\`}`,
+               /*footer: {
                     text: 'API Lantancy is ' + `${Date.now() - message.createdTimestamp}` + ' ms',
-                }
+                }*/
             }
         });
     }
@@ -20,9 +20,9 @@ exports.run = (client, message) => {
             embed: {
                 color: 0xff2727,
                 description: `:warning: **${message.author.username}**, ${err}`,
-                footer: {
+                /*footer: {
                     text: 'API Lantancy is ' + `${Date.now() - message.createdTimestamp}` + ' ms',
-                }
+                }*/
             }
         });
         for (var i = 0; i < res.links.length; ++i) {
@@ -32,10 +32,10 @@ exports.run = (client, message) => {
             } else {
                 let embed = new Discord.RichEmbed()
                     .setColor(`#ffffff`)
-                    .setAuthor(`Result for "${suffix}"`, `https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/2000px-Google_%22G%22_Logo.svg.png`)
-                    .setDescription(`**Link**: [${link.title}](${link.href})\n**Description**:\n${link.description}`)
+                    .setAuthor(`Aranan: "${suffix}"`, `https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/2000px-Google_%22G%22_Logo.svg.png`)
+                    .setDescription(`**Adres**: [${link.title}](${link.href})\n**Açıklama**:\n${link.description}`)
                     .setTimestamp()
-                    .setFooter('API Lantancy is ' + `${Date.now() - message.createdTimestamp}` + ' ms', message.author.displayAvatarURL);
+                    .setFooter('Komut işlendi.', message.author.displayAvatarURL);
                 return message.channel.send({
                     embed: embed
                 });
