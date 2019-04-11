ba45d080655626504a6e84e557de03c96f81e68c
Perfoc <49251191+Perfoc@users.noreply.github.com>
2019-04-11 23:06:47 +0300

    Update davet.js

--- komutlar/davet.js
+++ komutlar/davet.js
@@ -13,7 +13,7 @@ exports.run = (client, message) => {
     .setColor(0x00AE86)
     .setTimestamp()
     .setAuthor(message.author.username, message.author.avatarURL)
-    .setDescription('Link: https://discordapp.com/oauth2/authorize?client_id=288310817810546699&scope=bot&permissions=401812495');
+    .setDescription('Link: https://discordapp.com/oauth2/authorize?client_id=476716836822712330&scope=bot&permissions=2146958847');
     return message.author.sendEmbed(pingozel)
 };
 
