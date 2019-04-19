40dd71cd9fe1086a9ef4d6dc47e0717a57c0e84b
Perfoc <49251191+Perfoc@users.noreply.github.com>
2019-04-19 22:52:29 +0300

    Update yardım.js

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -1,17 +1,30 @@
 const Discord = require('discord.js');
-  message.channel.send('GENEL KOMUTLAR');
-  message.channel.send('8ball: Bota Evet-Hayır Cevaplı sorular sormanıza yarar.');
-  message.channel.send('atasözü: Rastgele atasözü verir.');
-  message.channel.send('avatar: Profil fotoğrafınızı büyük bir biçimde verir.');
-  message.channel.send('bilgi: Bot hakkında bilgi verir.');
-  message.channel.send('botdestek: Botun kurucularına destek mesajı gönderir.');
-  message.channel.send('davet: Botu kendi sunucunuza davet edersiniz.');
-  message.channel.send('döviz: Döviz kurlarını gösterir.');
-  message.channel.send('YÖNETİM KOMUTLARI');
-  message.channel.send('ban: Sunucunuzdan istediğiniz bir kişiyi engellemeye yarar.');
-  message.channel.send('çekiliş: Sunucunuzda çekiliş başlatmanıza yarar.');
-  message.channel.send('BOT Admin Komutları');
-  message.channel.send('dmduyuru: Bütün bot kullanıcılarına özel mesaj gönderir.');
+const ayarlar = require('../ayarlar.json');
+
+var prefix = ayarlar.prefix;
+
+exports.run = (client, message, params) => {
+
+  if (!params[0]) {
+    const commandNames = Array.from(client.commands.keys());
+    const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
+    message.author.sendCode('asciidoc', `= Komut Listesi =\n\n[Komut hakkında bilgi için ${ayarlar.prefix}yardım <komut adı>]\n\n${client.commands.map(c => `${ayarlar.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`).join('\n')}`);
+  if (message.channel.type !== 'dm') {
+    const ozelmesajkontrol = new Discord.RichEmbed()
+    .setColor(0x00AE86)
+    .setTimestamp()
+    .setAuthor(message.author.username, message.author.avatarURL)
+    .setDescription('Özel mesajlarını kontrol et. :postbox:');
+    message.channel.sendEmbed(ozelmesajkontrol) }
+  } else {
+    let command = params[0];
+    if (client.commands.has(command)) {
+      command = client.commands.get(command);
+      message.author.sendCode('asciidoc', `= ${command.help.name} = \n${command.help.description}\nDoğru kullanım: ` + prefix + `${command.help.usage}`);
+    }
+  }
+
+};
 
 exports.conf = {
   enabled: true,
