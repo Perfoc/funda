e10dda8dd13308e466adee68237b2defb8b20029
Perfoc <49251191+Perfoc@users.noreply.github.com>
2019-04-19 22:59:17 +0300

    Update yardım.js

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -8,7 +8,7 @@ exports.run = (client, message, params) => {
   if (!params[0]) {
     const commandNames = Array.from(client.commands.keys());
     const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
-    message.author.sendCode('asciidoc', `= Komut Listesi =\n\n[Komut hakkında bilgi için ${ayarlar.prefix}yardım <komut adı>]\n\n${client.commands.map(c => `${ayarlar.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`).join('\n')}`);
+    message.author.sendCode('GENEL KOMUTLAR\n8ball: Bota Evet-Hayır Cevaplı sorular sormanıza yarar.\natasözü: Rastgele atasözü verir.\navatar: Profil fotoğrafınızı büyük bir biçimde verir.\nbilgi: Bot hakkında bilgi verir.\nbotdestek: Botun kurucularına destek mesajı gönderir.\ndavet: Botu kendi sunucunuza davet edersiniz.\ndöviz: Döviz kurlarını gösterir.\nYÖNETİM KOMUTLARI\nban: Sunucunuzdan istediğiniz bir kişiyi engellemeye yarar.\nçekiliş: Sunucunuzda çekiliş başlatmanıza yarar.\nBOT Admin Komutları\ndmduyuru: Bütün bot kullanıcılarına özel mesaj gönderir.\n');
   if (message.channel.type !== 'dm') {
     const ozelmesajkontrol = new Discord.RichEmbed()
     .setColor(0x00AE86)
@@ -20,7 +20,7 @@ exports.run = (client, message, params) => {
     let command = params[0];
     if (client.commands.has(command)) {
       command = client.commands.get(command);
-      message.author.sendCode('GENEL KOMUTLAR\n8ball: Bota Evet-Hayır Cevaplı sorular sormanıza yarar.\natasözü: Rastgele atasözü verir.\navatar: Profil fotoğrafınızı büyük bir biçimde verir.\nbilgi: Bot hakkında bilgi verir.\nbotdestek: Botun kurucularına destek mesajı gönderir.\ndavet: Botu kendi sunucunuza davet edersiniz.\ndöviz: Döviz kurlarını gösterir.\nYÖNETİM KOMUTLARI\nban: Sunucunuzdan istediğiniz bir kişiyi engellemeye yarar.\nçekiliş: Sunucunuzda çekiliş başlatmanıza yarar.\nBOT Admin Komutları\ndmduyuru: Bütün bot kullanıcılarına özel mesaj gönderir.\n')
+      message.author.sendCode('GENEL KOMUTLAR\n8ball: Bota Evet-Hayır Cevaplı sorular sormanıza yarar.\natasözü: Rastgele atasözü verir.\navatar: Profil fotoğrafınızı büyük bir biçimde verir.\nbilgi: Bot hakkında bilgi verir.\nbotdestek: Botun kurucularına destek mesajı gönderir.\ndavet: Botu kendi sunucunuza davet edersiniz.\ndöviz: Döviz kurlarını gösterir.\nYÖNETİM KOMUTLARI\nban: Sunucunuzdan istediğiniz bir kişiyi engellemeye yarar.\nçekiliş: Sunucunuzda çekiliş başlatmanıza yarar.\nBOT Admin Komutları\ndmduyuru: Bütün bot kullanıcılarına özel mesaj gönderir.\n');
     }
   }
 
