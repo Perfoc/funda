bb01b0b00a6cd3d27afeeabbd545e656f9331900
Perfoc <49251191+Perfoc@users.noreply.github.com>
2019-04-19 22:42:03 +0300

    Update yardım.js

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -8,33 +8,31 @@ exports.run = (client, message, params) => {
   if (!params[0]) {
     const commandNames = Array.from(client.commands.keys());
     const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
-    message.author.sendCode('asciidoc', `= Komut Listesi =\n\n[Komut hakkında bilgi için ${ayarlar.prefix}yardım <komut adı>]\n\n${client.commands.map(c => `${ayarlar.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`).join('\n')}`);
   if (message.channel.type !== 'dm') {
     const ozelmesajkontrol = new Discord.RichEmbed()
     .setColor(0x00AE86)
     .setTimestamp()
     .setAuthor(message.author.username, message.author.avatarURL)
-    .setDescription('GENEL KOMUTLAR');
-    .setDescription('8ball: Bota Evet-Hayır Cevaplı sorular sormanıza yarar.');
-    .setDescription('atasözü: Rastgele atasözü verir.');
-    .setDescription('avatar: Profil fotoğrafınızı büyük bir biçimde verir.');
-    .setDescription('bilgi: Bot hakkında bilgi verir.');
-    .setDescription('botdestek: Botun kurucularına destek mesajı gönderir.');
-    .setDescription('davet: Botu kendi sunucunuza davet edersiniz.');
-    .setDescription('döviz: Döviz kurlarını gösterir.');
-    .setDescription('YÖNETİM KOMUTLARI');
-    .setDescription('ban: Sunucunuzdan istediğiniz bir kişiyi engellemeye yarar.');
-    .setDescription('çekiliş: Sunucunuzda çekiliş başlatmanıza yarar.');
-    .setDescription(': ');
-    .setDescription('BOT Admin Komutları');
-    .setDescription('dmduyuru: Bütün bot kullanıcılarına özel mesaj gönderir.');
+	.setDescription('Özel Mesajları Kontrol Et');
     
     message.channel.sendEmbed(ozelmesajkontrol) }
   } else {
     let command = params[0];
     if (client.commands.has(command)) {
       command = client.commands.get(command);
-      message.author.sendCode('asciidoc', `= ${command.help.name} = \n${command.help.description}\nDoğru kullanım: ` + prefix + `${command.help.usage}`);
+	message.author.sendCode('GENEL KOMUTLAR');
+    message.author.sendCode('8ball: Bota Evet-Hayır Cevaplı sorular sormanıza yarar.');
+    message.author.sendCode('atasözü: Rastgele atasözü verir.');
+    message.author.sendCode('avatar: Profil fotoğrafınızı büyük bir biçimde verir.');
+    message.author.sendCode('bilgi: Bot hakkında bilgi verir.');
+    message.author.sendCode('botdestek: Botun kurucularına destek mesajı gönderir.');
+    message.author.sendCode('davet: Botu kendi sunucunuza davet edersiniz.');
+    message.author.sendCode('döviz: Döviz kurlarını gösterir.');
+    message.author.sendCode('YÖNETİM KOMUTLARI');
+    message.author.sendCode('ban: Sunucunuzdan istediğiniz bir kişiyi engellemeye yarar.');
+    message.author.sendCode('çekiliş: Sunucunuzda çekiliş başlatmanıza yarar.');
+    message.author.sendCode('BOT Admin Komutları');
+    message.author.sendCode('dmduyuru: Bütün bot kullanıcılarına özel mesaj gönderir.');
     }
   }
 
