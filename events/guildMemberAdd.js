3771f7cb46e28342c78e1a6a253deb0a472ce0d8
Perfoc <49251191+Perfoc@users.noreply.github.com>
2019-04-26 18:46:23 +0300

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,5 +1,5 @@
 module.exports = member => {
     let username = member.user.username;
-    member.send('Hoş geldin **' + username + '**!');
+    //member.send('Hoş geldin **' + username + '**!');
     //member.guild.defaultChannel.send('hg '+username+'');
 };
