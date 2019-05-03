37e2bcd061338e0e9db5ff1164239eeb4b00e5f5
Perfoc <49251191+Perfoc@users.noreply.github.com>
2019-05-03 22:03:30 +0300

    Create afk.js

---
+++ komutlar/afk.js
@@ -0,0 +1,35 @@
+exports.run = (client, message, args) =>{
+    const setStatus = message.content.split(' ');
+
+    if(setStatus[1] === 'afk'){
+        client.user.setAFK(true);
+        message.channel.send("Your status has been set to afk!");
+    }
+
+    else if(setStatus[1] === 'notafk'){
+        client.user.setAFK(false);
+        message.channel.send(`Welcome back ${message.author}`);
+    }
+
+    else if(!setStatus[1] || setStatus[1] === undefined){
+        message.channel.send("You did not choose afk or notafk as current status!");
+    }
+
+    else{
+        message.channel.send("You did not choose afk or notafk as current status!");
+    }
+
+}
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['afk'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'afk',
+  description: 'AFK kaldığınızı belirtmenize yarar.',
+  usage: 'afk <sebep>'
+};
