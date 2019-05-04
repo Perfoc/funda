126d364427271b0372da98644176da08cb5f717d
Perfoc <49251191+Perfoc@users.noreply.github.com>
2019-05-04 18:07:07 +0300

    Update tagayarla.js

--- komutlar/tagayarla.js
+++ komutlar/tagayarla.js
@@ -3,7 +3,7 @@ const db = require('quick.db');
 
 exports.run = async (client, message, args) => {
  if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send('Bu komutu kullanmak için `Yönetici` yetkiniz olması lazım')
-  let tag = args.slice(1).join('' + '');
+  let tag = args.slice(0).join('');
   if (!tag) return message.channel.send('Bir tag girmelisin! \nGirdiysen Ama Kabul Etmemişse `?tagayarla` Yazıp ,Sonra Bir Şeyler Yazıp, Sonrasında İse Tagınızı Yazıp Tekrar Deneyin!\n**Örnek:** `?tagayarla a [TAG]`')
   db.set(`tags_${message.guild.id}`, tag)
   message.channel.send('Tag başarıyla `'+ tag +'` olarak ayarlandı!')
